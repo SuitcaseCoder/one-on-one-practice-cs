729c26d514509dca38a0a752d3ffda49f6bcdc85
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-22 09:18:51 -0500

    java

---
+++ java/src/June22.java
@@ -0,0 +1,32 @@
+public class June22 {
+    public static void main(String[] args) {
+    
+//       print out "hello, nice to meet you" , then print your name in a
+//       new line
+        System.out.println("hello, nice to meet you");
+        System.out.println("Laura");
+        
+//        print the sum of two numbers
+        System.out.println(72 + 24);
+    
+
+        
+        
+
+////        Create a function that takes two integers and checks if they are equal.
+////        ex: isEqual(4,8) // false, isEqual(3,3) // true
+////    solution:
+//        public static boolean isEqual(int num1, int num2) {
+//		return num1==num2;
+//  }
+//
+//
+////    Create a method that takes two integers as arguments and return their sum.
+////        ex: AddTwoNums(3,2) //
+//
+//        public static int sum(int a, int b) {
+//            return a + b;
+//        }
+//
+    }
+}
