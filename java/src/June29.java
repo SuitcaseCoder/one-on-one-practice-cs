2b82110ec93c2f8847430a13f2bb71b19431364b
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-29 12:07:54 -0500

    June29

---
+++ java/src/June29.java
@@ -0,0 +1,10 @@
+public class June29 {
+
+}
+
+//TODO: prompt a user to tell us about themselves (age, name, if they
+// have any pets (true/false). Using method overloading, create methods
+// that will return the first fact they told us about themselves. ex: I can input: 18 --
+// Laura -- false in which case I expect you to return: 18. but if I
+// tell you my name first: Laura -- false -- 18. I expect you to return
+// "Laura" ... etc.
